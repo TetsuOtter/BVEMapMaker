417a41be4b6e9fc0454273d961cb76fdf4a69ffa
Tetsu Otter <tetsu_otter@aol.com>
2018-09-17 19:47:06 +0900

    プロジェクト ファイルを追加します。

---
+++ BVEMapMaker/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BVEMapMaker
+{
+    public class Class1
+    {
+    }
+}
