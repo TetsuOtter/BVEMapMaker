6264baf7512678d0ae755a3651a38838806b881c
Tetsu Otter <tetsu_otter@aol.com>
2018-09-27 22:50:47 +0900

    TrackCreateクラスの準備

--- BVEMapMaker/Class1.cs
+++ BVEMapMaker/Class1.cs
@@ -5,12 +5,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace TRtec.BVEMapMaker
 {
+  /// <summary>
+  /// BVE Mapファイル作成用クラス
+  /// </summary>
   public class Map
   {
     //改行は自動挿入
+    /// <summary>
+    /// Mapクラスのインスタンスを初期化
+    /// </summary>
+    /// <param name="VersionNum">クラスバージョン情報</param>
     public Map(int VersionNum)
     {
       VNum = VersionNum;
@@ -44,9 +50,9 @@ namespace TRtec.BVEMapMaker
     static string BVEMapHeader;
     int VNum = new int();
     /// <summary>
-    /// BVEMapMakerを初期化します。
+    /// Mapファイルを出力する
     /// </summary>
-    /// <param name="VersionNum">MapMakerの利用バージョン</param>
+    /// <param name="MapName">マップファイル名</param>
     public void WriteMap(string MapName)
     {
       using(StreamWriter sw=new StreamWriter(MapName, false, Encoding.UTF8))
@@ -271,7 +277,6 @@ namespace TRtec.BVEMapMaker
       /// </summary>
       /// <param name="D">距離程[m]</param>
       /// <param name="TN">軌道名</param>
-      /// <param name="X">X軸方向位置[m]</param>
       public void Position(double D, string TN) => Position(D, TN, 0, 0, 0, 0);
       /// <summary>
       /// 指定の軌道について、指定の距離程におけるX軸方向位置を設定する(Y軸方向位置と各曲線相対半径は0[m])
@@ -379,6 +384,7 @@ namespace TRtec.BVEMapMaker
         /// 指定の地点でカントを終了する
         /// </summary>
         /// <param name="D">終了する地点</param>
+        /// <param name="TN">軌道名</param>
         public void End(double D, string TN) => ListAdd(D, "Track['" + TN + "'].Cant.End();");
 
         /// <summary>
@@ -391,6 +397,7 @@ namespace TRtec.BVEMapMaker
         /// 指定の地点におけるカントを設定する(構文間の数値は補完される)
         /// </summary>
         /// <param name="D">距離程[m]</param>
+        /// <param name="TN">軌道名</param>
         /// <param name="C">カント[m](半径と正負を一致させる)</param>
         public void Interpolate(double D, string TN, double C) => ListAdd(D, "Track['" + TN + "'].Cant.Interpolate(" + C.ToString() + ");");
       }
@@ -689,8 +696,8 @@ namespace TRtec.BVEMapMaker
         Curve c = new Curve();
         c.Interpolate(BTCDistance, 0, 0);
         c.Interpolate(BTCDistance + BTCL, Radius, Cant);
-        c.Interpolate(BTCDistance + BTCL + CCL, Radius, Cant);
-        c.Interpolate(BTCDistance + BTCL + CCL + ETCL, Radius, Cant);
+        c.Interpolate(BTCDistance + BTCL + CCL);
+        c.Interpolate(BTCDistance + BTCL + CCL + ETCL, 0, 0);
       }
 
       /// <summary>
@@ -707,49 +714,122 @@ namespace TRtec.BVEMapMaker
       public void GradientChange(double GradSignDist, double StartGrad, double EndGrad, double Radius)
       {
         Gradient g = new Gradient();
-        double Long = Radius * Math.Sin(Math.Atan((StartGrad - EndGrad) / (1 + (StartGrad * EndGrad))));
-        if (StartGrad == 0)
-        {
-          g.BeginTransition(GradSignDist - (Long / 2));
-          g.Begin(GradSignDist + (Long / 2), EndGrad);
-        }else if (EndGrad == 0)
-        {
-          g.BeginTransition(GradSignDist - (Long / 2));
-          g.End(GradSignDist + (Long / 2));
-        }
-        else
-        {
-          double slong = Long * StartGrad / (StartGrad + EndGrad);
-          double elong = Long * EndGrad / (StartGrad + EndGrad);
-          g.BeginTransition(GradSignDist - slong);
-          g.Begin(GradSignDist + elong, EndGrad);
-        }
+        double Theta = Math.Atan((Math.Abs(StartGrad) + Math.Abs(EndGrad)) / 1000);
+        double Long = Radius * Theta;
+        Console.WriteLine("GradTransLong = " + Long.ToString());
+        g.BeginTransition(GradSignDist - (Long / 2));
+        g.Begin(GradSignDist + (Long / 2), EndGrad);
       }
 
       /// <summary>
       /// 1線から2線へ分岐する分岐器を設置する。(自線がカーブしている場合は不具合が発生する可能性あり)
       /// </summary>
-      /// <param name="StartDist">開始地点の距離程[m]</param>
-      /// <param name="CurveTrackName">分岐側軌道名(自線不可)</param>
-      /// <param name="StraightTrackName">直線側軌道名(0で自線)</param>
+      /// <param name="StartDist">分岐器開始位置[m]</param>
+      /// <param name="CurveTrack">分岐側軌道情報</param>
+      /// <param name="StraightTrack">直線側軌道情報</param>
+      /// <param name="Num">分岐器の番数</param>
+      /// <param name="IsCurveLeft">分岐側軌道が左方向に分岐するかどうか</param>
+      /// <exception cref="ArgumentException">分岐器番数に0は指定できません。</exception>
+      /// <exception cref="ArgumentException">分岐側軌道に自線は指定できません。</exception>
+      /// <exception cref="ArgumentException">直線側軌道と分岐側軌道は同じにできません。</exception>
+      /// <exception cref="ArgumentException">直線側軌道と分岐側軌道は同じ傾きである必要があります。</exception>
+      public SWInfo One4TwoSWPut(double StartDist, TrackInfo CurveTrack, TrackInfo StraightTrack, bool IsCurveLeft, double Num) => One4TwoSWPut(StartDist, CurveTrack, StraightTrack, IsCurveLeft, Num, 1.067);
+      /// <summary>
+      /// 1線から2線へ分岐する分岐器を設置する。(自線がカーブしている場合は不具合が発生する可能性あり)
+      /// </summary>
+      /// <param name="StartDist">分岐器開始位置[m]</param>
+      /// <param name="CurveTrack">分岐側軌道情報</param>
+      /// <param name="StraightTrack">直線側軌道情報</param>
       /// <param name="Num">分岐器の番数</param>
       /// <param name="Gauge">軌間[m]</param>
-      public SWInfo One4TwoSWPut(double StartDist, TrackInfo CurveTrack, TrackInfo StraightTrack, int Num, double Gauge)
+      /// <param name="IsCurveLeft">分岐側軌道が左方向に分岐するかどうか</param>
+      /// <exception cref="ArgumentException">分岐器番数に0は指定できません。</exception>
+      /// <exception cref="ArgumentException">分岐側軌道に自線は指定できません。</exception>
+      /// <exception cref="ArgumentException">直線側軌道と分岐側軌道は同じにできません。</exception>
+      /// <exception cref="ArgumentException">直線側軌道と分岐側軌道は同じ傾きである必要があります。</exception>
+      public SWInfo One4TwoSWPut(double StartDist, TrackInfo CurveTrack, TrackInfo StraightTrack, bool IsCurveLeft, double Num, double Gauge)
       {
-        if (CurveTrack.TrackName == "0") throw new Exception("分岐側軌道に自線を設定することはできません。");
-        double Radius = 2 * Gauge * (Num ^ 2);
         SWInfo sw = new SWInfo();
-        sw.DistBetweenSAndC = 0;
+        Track t = new Track();
+        if (CurveTrack.TrackName == "0") throw new Exception("分岐側軌道に自線を設定することはできません。", new ArgumentException());
+        if (Num == 0) throw new Exception("分岐器番数に「０」を指定することはできません。", new ArgumentException());
+        if (CurveTrack.TrackName == StraightTrack.TrackName) throw new Exception("直線側軌道と分岐側軌道は同じにできません。", new ArgumentException());
+        if (CurveTrack.Slope != StraightTrack.Slope) throw new Exception("直線側軌道と分岐側軌道は同じ傾きである必要があります。", new ArgumentException());
+        Num = Math.Abs(Num);
+        double Theta = 2 * Math.Atan(1 / (2 * Num));
+        double Alpha = Math.Atan(StraightTrack.Slope);
+        double Radius = Gauge * ((1 / (1 - Math.Cos(Theta))) - 0.5);
+        sw.SWLength = Radius * Math.Tan(Theta / 2) * (1 + Math.Cos(Theta) - Math.Sin(Theta) * Math.Tan(Alpha)) * Math.Cos(Alpha);
+        if (IsCurveLeft) Radius = -Radius;
+        if (CurveTrack.TrackName != "0") t.XInterpolate(StartDist, StraightTrack.TrackName, StraightTrack.Location[0], 0);
+        t.XInterpolate(StartDist, CurveTrack.TrackName, CurveTrack.Location[0], Radius);
+        sw.DistBetweenSAndC = Radius * Math.Tan(Theta / 2) * (1 + Math.Sin(Theta));
+        sw.StartDistance = StartDist;
+        sw.StraightEnd = StraightTrack;
+        sw.CurveEnd = CurveTrack;
+        sw.StraightEnd.Location[0] = StraightTrack.Location[0] + sw.SWLength * Math.Tan(Alpha);
+        sw.CurveEnd.Location[0] = sw.StraightEnd.Location[0] + Radius * Math.Sin(Theta) * Math.Tan(Theta / 2) / Math.Cos(Alpha);
+        if (IsCurveLeft) sw.CurveEnd.Slope -= 1 / Num;
+        else sw.CurveEnd.Slope += 1 / Num;
+
+        t.XInterpolate(sw.SWLength + StartDist, StraightTrack.TrackName, sw.StraightEnd.Location[0], 0);
+        if (CurveTrack.TrackName != "0") t.XInterpolate(sw.SWLength + StartDist, CurveTrack.TrackName, sw.CurveEnd.Location[0], 0);
+
         return sw;
       }
+      /// <summary>
+      /// 他線に関する情報
+      /// </summary>
       public class TrackInfo
       {
+
+        /// <summary>
+        /// TrackInfoを初期化
+        /// </summary>
+        /// <param name="TN">軌道名</param>
+        public TrackInfo(string TN)
+        {
+          TrackName = TN;
+        }
+        /// <summary>
+        /// TrackInfoを初期化
+        /// </summary>
+        /// <param name="TN">軌道名</param>
+        /// <param name="LocationArray">軌道位置座標情報(x,y)[m]</param>
+        public TrackInfo(string TN, double[] LocationArray)
+        {
+          TrackName = TN;
+          Location = LocationArray.Take(2).ToArray();
+        }
+        /// <summary>
+        /// TrackInfoを初期化
+        /// </summary>
+        /// <param name="TN">軌道名</param>
+        /// <param name="LocationArray">軌道位置座標情報(x,y)[m]</param>
+        /// <param name="SlopeNum">軌道の傾き情報(dz/dx)</param>
+        public TrackInfo(string TN, double[] LocationArray, double SlopeNum)
+        {
+          TrackName = TN;
+          Location = LocationArray.Take(2).ToArray();
+          Slope = SlopeNum;
+        }
+
         /// <summary>
         /// 軌道名
         /// </summary>
         public string TrackName;
-
+        /// <summary>
+        /// 軌道位置(座標(x,y,z))
+        /// </summary>
+        public double[] Location = new double[2];
+        /// <summary>
+        /// 自線に対する傾き(dz/dx)
+        /// </summary>
+        public double Slope;
       }
+      /// <summary>
+      /// 分岐器関数の処理結果
+      /// </summary>
       public class SWInfo
       {
         /// <summary>
@@ -761,9 +841,124 @@ namespace TRtec.BVEMapMaker
         /// </summary>
         public double StartDistance;
         /// <summary>
-        /// 分岐器関数終了距離
+        /// 分岐線処理終了時の分岐線側軌道の状態
         /// </summary>
-        public double EndDistance;
+        public TrackInfo CurveEnd;
+        /// <summary>
+        /// 直線側処理終了時の直線側軌道の状態
+        /// </summary>
+        public TrackInfo StraightEnd;
+        /// <summary>
+        /// 分岐器カーブ長さ(直線基準)
+        /// </summary>
+        public double SWLength;
+      }
+
+      /// <summary>
+      /// 他線を自動で(力技で)真っすぐに配置してくれる便利な、とーっても便利な関数(TCLの半分とCCLの合計が90度以上の弧の長さに相当する場合は非対応)
+      /// </summary>
+      /// <param name="mci">自線の曲線情報</param>
+      /// <param name="ti">真っすぐに配置したい他線の情報</param>
+      /// <param name="Accurate">緩和曲線分割数[m](大きければ大きいほど直線に近くなります 0の場合は自動で50が代入されます)</param>
+      public void KeepStraight(MyCurveInfo mci, ref TrackInfo ti, double Accurate)
+      {
+        if (Accurate < 0) throw new ArgumentOutOfRangeException("Accurate", Accurate.ToString(), "負の値を入れることはできません。");
+        if (ti.TrackName == "0") throw new ArgumentException("他線に自線を指定することはできません。", "ti.TrackName");
+        if (ti.TrackName == null) throw new ArgumentNullException("ti.TrackName", "他線の名前がnullです。");
+        if (ti.TrackName == string.Empty) throw new ArgumentOutOfRangeException("ti.TrackName", "他線の名前が指定されていません。");
+        if (Accurate == 0) Accurate = 50;
+        double CutDistance = mci.STCL / Accurate;
+        double NowZLocation = 0;
+        double AbsR = Math.Abs(mci.Radius);
+
+        double TCL = mci.STCL;
+        double TCL2l2RPI2 = (TCL * TCL) / (2 * AbsR * Math.PI * Math.PI);
+        double NowX = Calc(0);
+
+
+        double Calc(double Nowz)
+        {
+          return (Nowz * Nowz / (4 * mci.Radius)) - TCL2l2RPI2 * (1 - Math.Cos(Nowz * Math.PI / TCL));
+        }
+      }
+
+      /// <summary>
+      /// 自線の曲線情報についてのクラス
+      /// </summary>
+      public class MyCurveInfo
+      {
+        /// <summary>
+        /// MyCurveInfoを曲線情報変化位置情報を用いて初期化する
+        /// </summary>
+        /// <param name="R">円曲線の半径[m]</param>
+        /// <param name="BTC">緩和曲線開始位置[m]</param>
+        /// <param name="BCC">円曲線開始位置[m]</param>
+        /// <param name="ECC">円曲線終了位置[m]</param>
+        /// <param name="ETC">緩和曲線終了位置[m]</param>
+        public MyCurveInfo(double R, double BTC, double BCC, double ECC, double ETC)
+        {
+          Radius = R;
+          BTCDistance = BTC;
+          BCCDistance = BCC;
+          ECCDistance = ECC;
+          ETCDistance = ETC;
+          STCL = BCC - BTC;
+          CCL = ECC - BCC;
+          ETCL = ETC - ECC;
+        }
+
+        internal double Radius;
+        internal double BTCDistance;
+        internal double BCCDistance;
+        internal double ECCDistance;
+        internal double ETCDistance;
+        internal double STCL;
+        internal double CCL;
+        internal double ETCL;
+      }
+
+      /// <summary>
+      /// 他線を設定するクラス
+      /// </summary>
+      public class TrackCreate
+      {
+        static private List<string> TrackNameList = new List<string>();
+        private string TrackName { get; }
+        private List<double[]> Location = new List<double[]>();//Z,X,Y
+
+        /// <summary>
+        /// 他線を新しく定義する
+        /// </summary>
+        /// <param name="Name">軌道名</param>
+        /// <param name="X">初期X座標[m]</param>
+        /// <param name="Y">初期Y座標[m]</param>
+        /// <param name="Gauge">軌間[m]</param>
+        public TrackCreate(string Name, double X, double Y, double Gauge)
+        {
+          if (Name == null) throw new ArgumentNullException("Name", "軌道名にnullを指定することはできません。");
+          else if (Name == string.Empty) throw new ArgumentException("軌道名がEmptyです。", "Name");
+          else if (Name == "0") throw new ArgumentException("軌道名「0」は自軌道を表す文字として予約されています。", "Name");
+          else
+          {
+            Exception e = null;
+            try
+            {
+              TrackNameList.Find(n => n == Name);
+            }
+            catch (ArgumentNullException ex)
+            {
+              e = ex;
+              TrackName = Name;
+              TrackNameList.Add(Name);
+            }
+            finally
+            {
+              if (e == null) throw new ArgumentException("軌道名'" + Name + "'は既に使用されています。", "Name");
+            }
+          }
+
+        }
+
       }
     }
 
